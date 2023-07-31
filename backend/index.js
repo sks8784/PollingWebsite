0661418b7e1e31ab113760ee3e71ae523b8905e1
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-08-01 02:34:31 +0530

    cors resolved

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const cors=require('cors');
 // app.use(express.json());
 app.use(express.json({limit: '50mb'}));
 app.use(cors({
-    "origin": ["http://localhost:3000"],
+    "origin": ["http://localhost:3000","https://zesty-churros-71cba2.netlify.app/"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }));
