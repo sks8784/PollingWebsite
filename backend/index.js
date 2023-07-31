b9ae1d8f83f53e11b71cc61f4cc6c24badfafa0d
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-08-01 02:59:09 +0530

    cors changed

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,7 @@ const PORT=process.env.PORT;
 const cors=require('cors');
 // app.use(express.json());
 app.use(express.json({limit: '50mb'}));
-app.use(cors({
-    "origin": ["http://localhost:3000","https://zesty-churros-71cba2.netlify.app/","https://pollingapi-rz7e.onrender.com/"],
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-}));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 
 const mongoURL=process.env.MONGO_URL;
