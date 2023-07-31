0d93e546cd53c60c95003c8cf74512aef8a875ff
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-08-01 01:58:44 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 const express=require('express');
+const app=express();
+
 const bodyParser=require('body-parser');
 const dotenv=require('dotenv');
 dotenv.config();
-const app=express();
+
 const mongoose=require('mongoose');
 const PORT=process.env.PORT;
-// const cors=require('cors');
+const cors=require('cors');
 // app.use(express.json());
-// app.use(cors());
 app.use(express.json({limit: '50mb'}));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 
 const mongoURL=process.env.MONGO_URL;
