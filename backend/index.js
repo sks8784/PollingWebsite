8453d0e0b7a1192dedf1e697847ab7ba354caee8
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-08-01 01:16:22 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const dotenv=require('dotenv');
 dotenv.config();
 const app=express();
 const mongoose=require('mongoose');
 const PORT=process.env.PORT;
-const cors=require('cors');
-app.use(express.json());
-app.use(cors());
+// const cors=require('cors');
+// app.use(express.json());
+// app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 const mongoURL=process.env.MONGO_URL;
 mongoose.connect(mongoURL, 
