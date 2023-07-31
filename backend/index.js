6edbd69f63d5a7faafda9a69cfa62ddac1faf2d7
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-08-01 02:44:04 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const cors=require('cors');
 // app.use(express.json());
 app.use(express.json({limit: '50mb'}));
 app.use(cors({
-    "origin": ["http://localhost:3000","https://zesty-churros-71cba2.netlify.app/"],
+    "origin": ["http://localhost:3000","https://zesty-churros-71cba2.netlify.app/","https://pollingapi-rz7e.onrender.com/"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }));
